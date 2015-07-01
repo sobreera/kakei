1b1521998ac13379eb418f5d5b7d4d0dbaaf428c
宇野嵐 <araera48@gmail.com>
2015-07-01 19:38:20 +0900

    初コミットです

--- app/src/main/java/com/example/era/kakei/GraphActivity.java
+++ app/src/main/java/com/example/era/kakei/GraphActivity.java
@@ -12,6 +12,8 @@ public class GraphActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
+
+        //グラフ作成用にActivity作成したもののまだよくわかってないので作っただけ状態
     }
 
     @Override
