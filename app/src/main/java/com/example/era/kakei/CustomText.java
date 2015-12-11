41d98e48d171e1abe6f1c7049731c78c5071248a
宇野嵐 <araera48@gmail.com>
2015-12-11 20:21:27 +0900

    1211

---
+++ app/src/main/java/com/example/era/kakei/CustomText.java
@@ -0,0 +1,26 @@
+package com.example.era.kakei;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.widget.TextView;
+
+/**
+ * Created by era on 15/12/11.
+ */
+public class CustomText extends TextView {
+
+    public CustomText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void setText(CharSequence text, BufferType type) {
+        String get = text.toString();
+        String parseText="";
+        for (int i = 0; i < get.length(); i++) {
+            parseText += get.charAt(i)+"\n";
+        }
+        super.setText(parseText, type);
+    }
+}
